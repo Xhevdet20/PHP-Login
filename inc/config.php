c22ed112833cf5fecb4cbe246536122bb48938e3
Xhevdet Kryeziu <xhevdet.kryeziu@kujtesa.com>
2019-12-06 09:58:19 +0100

    Added config: added login/reg pages added footer

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// If there's no contsant defined called __CONFIG___ do  not load the file
+if (!defined('__CONFIG__')) {
+    exit("You do not have a config file");
+}
+
+
+// Our config is below
